8e48dfe3935046b17472efa3a3ed1c6bf90f762b
Adam Perry <adam.n.perry@gmail.com>
2018-04-04 22:37:26 -0700

    Remove outdated comment.

--- wrapper.h
+++ wrapper.h
@@ -1,5 +1,1 @@
-// support development on non-linux platforms,
-// but this'll need to be run on a linux box
-// so we should try to use the correct version
-// for where this is being built
 #include <linux/perf_event.h>
