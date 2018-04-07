74838497439c8fd28edfff9ae16a2e13eb614c18
Adam Perry <adam.n.perry@gmail.com>
2018-04-07 00:20:44 -0700

    failure for error handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,10 @@
 extern crate errno;
+extern crate failure;
+#[macro_use]
+extern crate failure_derive;
 extern crate libc;
 
 // TODO logging
-// TODO better error handling
 
 use std::collections::{BTreeMap, BTreeSet};
 use std::fs::File;
@@ -24,7 +26,14 @@ impl Counts {
         }
     }
 
-    // TODO ioctl enable
+    pub fn start(&mut self) -> Vec<Result<(), Errno>> {
+        // TODO ioctl enable -- enable_on_exec?
+        unimplemented!();
+    }
+
+    pub fn read(&mut self) -> Vec<(Event, u64)> {
+        unimplemented!();
+    }
 }
 
 pub struct CountsBuilder {
@@ -44,7 +53,7 @@ impl CountsBuilder {
         self
     }
 
-    pub fn init(self) -> (Result<Counts, ()>, Result<(), BTreeMap<Event, Errno>>) {
+    pub fn init(self) -> (Result<Counts, ()>, Result<(), BTreeMap<Event, OpenError>>) {
         let mut counters = Vec::new();
         let mut failures = BTreeMap::new();
 
@@ -110,7 +119,7 @@ struct EventCounter {
 }
 
 impl EventCounter {
-    fn new(event: Event, pid: PidConfig, cpu: CpuConfig) -> Result<Self, Errno> {
+    fn new(event: Event, pid: PidConfig, cpu: CpuConfig) -> Result<Self, OpenError> {
         let file = unsafe { File::from_raw_fd(event.create_fd(pid, cpu)?) };
         Ok(Self { event, file })
     }
@@ -123,8 +132,115 @@ pub enum Event {
     HardwareCache(CacheId, CacheOpId, CacheOpResultId),
 }
 
+#[derive(Debug, Fail)]
+pub enum OpenError {
+    #[fail(display = "Returned if the perf_event_attr size value is too small
+              (smaller than PERF_ATTR_SIZE_VER0), too big (larger than the
+              page size), or larger than the kernel supports and the extra
+              bytes are not zero.  When E2BIG is returned, the
+              perf_event_attr size field is overwritten by the kernel to be
+              the size of the structure it was expecting.")]
+    AttrWrongSize,
+    #[fail(display = "Returned when the requested event requires CAP_SYS_ADMIN
+              permissions (or a more permissive perf_event paranoid
+              setting).  Some common cases where an unprivileged process may
+              encounter this error: attaching to a process owned by a
+              different user; monitoring all processes on a given CPU (i.e.,
+              specifying the pid argument as -1); and not setting
+              exclude_kernel when the paranoid setting requires it.")]
+    CapSysAdminRequired,
+    #[fail(display = "Returned if the group_fd file descriptor is not valid, or, if
+              PERF_FLAG_PID_CGROUP is set, the cgroup file descriptor in pid
+              is not valid.")]
+    InvalidFdOrPid,
+    #[fail(display = "Returned if another event already has exclusive access to the
+              PMU.")]
+    PmuBusy,
+    #[fail(display = "Returned if the attr pointer points at an invalid memory
+              address.")]
+    AttrInvalidPointer,
+    #[fail(display = "Returned if the specified event is invalid.  There are many
+              possible reasons for this.  A not-exhaustive list: sample_freq
+              is higher than the maximum setting; the cpu to monitor does
+              not exist; read_format is out of range; sample_type is out of
+              range; the flags value is out of range; exclusive or pinned
+              set and the event is not a group leader; the event config
+              values are out of range or set reserved bits; the generic
+              event selected is not supported; or there is not enough room
+              to add the selected event.")]
+    InvalidEvent,
+    #[fail(display = "Each opened event uses one file descriptor.  If a large number
+              of events are opened, the per-process limit on the number of
+              open file descriptors will be reached, and no more events can
+              be created.")]
+    TooManyOpenFiles,
+    #[fail(display = "Returned when the event involves a feature not supported by
+              the current CPU.")]
+    CpuFeatureUnsupported,
+    #[fail(display = "Returned if the type setting is not valid.  This error is also
+              returned for some unsupported generic events.")]
+    InvalidEventType,
+    #[fail(display = "Prior to Linux 3.3, if there was not enough room for the
+              event, ENOSPC was returned.  In Linux 3.3, this was changed to
+              EINVAL.  ENOSPC is still returned if you try to add more
+              breakpoint events than supported by the hardware.")]
+    TooManyBreakpoints,
+    #[fail(display = "Returned if PERF_SAMPLE_STACK_USER is set in sample_type and
+              it is not supported by hardware.")]
+    UserStackSampleUnsupported,
+    #[fail(display = "Returned if an event requiring a specific hardware feature is
+              requested but there is no hardware support.  This includes
+              requesting low-skid events if not supported, branch tracing if
+              it is not available, sampling if no PMU interrupt is
+              available, and branch stacks for software events.")]
+    HardwareFeatureUnsupported,
+    #[fail(display = "(since Linux 4.8)
+              Returned if PERF_SAMPLE_CALLCHAIN is requested and
+              sample_max_stack is larger than the maximum specified in
+              /proc/sys/kernel/perf_event_max_stack.")]
+    SampleMaxStackTooLarge,
+    #[fail(display = "Returned on many (but not all) architectures when an
+              unsupported exclude_hv, exclude_idle, exclude_user, or
+              exclude_kernel setting is specified.
+
+              It can also happen, as with EACCES, when the requested event
+              requires CAP_SYS_ADMIN permissions (or a more permissive
+              perf_event paranoid setting).  This includes setting a
+              breakpoint on a kernel address, and (since Linux 3.13) setting
+              a kernel function-trace tracepoint.")]
+    CapSysAdminRequiredOrExcludeUnsupported,
+    #[fail(display = "Returned if attempting to attach to a process that does not
+              exist.")]
+    ProcessDoesNotExist,
+    #[fail(display = "The kernel returned an unexpected error code: {}", errno)]
+    Unknown { errno: Errno },
+}
+
+impl From<Errno> for OpenError {
+    fn from(errno: Errno) -> OpenError {
+        match errno.0 {
+            libc::E2BIG => OpenError::AttrWrongSize,
+            libc::EACCES => OpenError::CapSysAdminRequired,
+            libc::EBADF => OpenError::InvalidFdOrPid,
+            libc::EBUSY => OpenError::PmuBusy,
+            libc::EFAULT => OpenError::AttrInvalidPointer,
+            libc::EINVAL => OpenError::InvalidEvent,
+            libc::EMFILE => OpenError::TooManyOpenFiles,
+            libc::ENODEV => OpenError::CpuFeatureUnsupported,
+            libc::ENOENT => OpenError::InvalidEventType,
+            libc::ENOSPC => OpenError::TooManyBreakpoints,
+            libc::ENOSYS => OpenError::UserStackSampleUnsupported,
+            libc::EOPNOTSUPP => OpenError::HardwareFeatureUnsupported,
+            libc::EOVERFLOW => OpenError::SampleMaxStackTooLarge,
+            libc::EPERM => OpenError::CapSysAdminRequiredOrExcludeUnsupported,
+            libc::ESRCH => OpenError::ProcessDoesNotExist,
+            _ => OpenError::Unknown { errno },
+        }
+    }
+}
+
 impl Event {
-    fn create_fd(&self, pid: PidConfig, cpu: CpuConfig) -> Result<RawFd, Errno> {
+    fn create_fd(&self, pid: PidConfig, cpu: CpuConfig) -> Result<RawFd, OpenError> {
         unsafe {
             match syscall(
                 SYS_perf_event_open,
@@ -141,7 +257,7 @@ impl Event {
                 // support yet
                 0,
             ) {
-                -1 => Err(errno()),
+                -1 => Err(errno().into()),
                 fd => Ok(fd as RawFd),
             }
         }
